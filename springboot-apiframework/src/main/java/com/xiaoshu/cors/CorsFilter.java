89112502b5e90f9a8c1482e136cf843f6e9a9772
deane163 <deane163@126.com>
2017-12-15 17:15:17 +0800

    【功能】添加基础jar包

---
+++ springboot-apiframework/src/main/java/com/xiaoshu/cors/CorsFilter.java
@@ -0,0 +1,95 @@
+package com.xiaoshu.cors;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.CollectionUtils;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 
+ * code is far away from bug with the animal protecting
+ * ┏┓　　　┏┓
+ * ┏┛┻━━━┛┻┓
+ * ┃　　　　　　　┃
+ * ┃　　　━　　　┃
+ * ┃　┳┛　┗┳　┃
+ * ┃　　　　　　　┃
+ * ┃　　　┻　　　┃
+ * ┃　　　　　　　┃
+ * ┗━┓　　　┏━┛
+ * 　　┃　　　┃神兽保佑
+ * 　　┃　　　┃代码无BUG！
+ * 　　┃　　　┗━━━┓
+ * 　　┃　　　　　　　┣┓
+ * 　　┃　　　　　　　┏┛
+ * 　　┗┓┓┏━┳┓┏┛
+ * 　　　┃┫┫　┃┫┫
+ * 　　　┗┻┛　┗┻┛
+ *
+ *
+ * @Description : 支持 CORS 跨域
+ * ---------------------------------
+ * @Author : deane.administrator
+ * @Date : Create in 2017年12月15日下午4:53:28
+ * 
+ * Copyright (C)2013-2017 小树盛凯科技 All rights reserved.
+ */
+public class CorsFilter implements Filter {
+
+    private String allowOrigin;
+    private String allowMethods;
+    private String allowCredentials;
+    private String allowHeaders;
+    private String exposeHeaders;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        allowOrigin = filterConfig.getInitParameter("allowOrigin");
+        allowMethods = filterConfig.getInitParameter("allowMethods");
+        allowCredentials = filterConfig.getInitParameter("allowCredentials");
+        allowHeaders = filterConfig.getInitParameter("allowHeaders");
+        exposeHeaders = filterConfig.getInitParameter("exposeHeaders");
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+    	System.out.println("解决跨域问题");
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+        if (StringUtils.isNotEmpty(allowOrigin)) {
+            List<String> allowOriginList = Arrays.asList(allowOrigin.split(","));
+            if (!CollectionUtils.isEmpty(allowOriginList)) {
+                String currentOrigin = request.getHeader("Origin");
+                if (allowOriginList.contains(currentOrigin)) {
+                    response.setHeader("Access-Control-Allow-Origin", currentOrigin);
+                }
+            }
+        }
+        if (StringUtils.isNotEmpty(allowMethods)) {
+            response.setHeader("Access-Control-Allow-Methods", allowMethods);
+        }
+        if (StringUtils.isNotEmpty(allowCredentials)) {
+            response.setHeader("Access-Control-Allow-Credentials", allowCredentials);
+        }
+        if (StringUtils.isNotEmpty(allowHeaders)) {
+            response.setHeader("Access-Control-Allow-Headers", allowHeaders);
+        }
+        if (StringUtils.isNotEmpty(exposeHeaders)) {
+            response.setHeader("Access-Control-Expose-Headers", exposeHeaders);
+        }
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void destroy() {
+    	//省略
+    }
+}
