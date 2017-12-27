d7977fd517467c4461f912de8d3982b0172ce919
deane163 <deane163@126.com>
2017-12-27 16:03:10 +0800

    【功能】添加日志实现服务类接口声明

---
+++ springboot-apiframework/src/main/java/com/xiaoshu/service/LogService.java
@@ -0,0 +1,38 @@
+package com.xiaoshu.service;
+
+import java.util.Date;
+
+/**
+ * 
+ * code is far away from bug with the animal protecting
+ * ┏┓　　　┏┓
+ * ┏┛┻━━━┛┻┓
+ * ┃　　　　　　　┃
+ * ┃　　　━　　　┃
+ * ┃　┳┛　┗┳　┃
+ * ┃　　　　　　　┃
+ * ┃　　　┻　　　┃
+ * ┃　　　　　　　┃
+ * ┗━┓　　　┏━┛
+ * 　　┃　　　┃神兽保佑
+ * 　　┃　　　┃代码无BUG！
+ * 　　┃　　　┗━━━┓
+ * 　　┃　　　　　　　┣┓
+ * 　　┃　　　　　　　┏┛
+ * 　　┗┓┓┏━┳┓┏┛
+ * 　　　┃┫┫　┃┫┫
+ * 　　　┗┻┛　┗┻┛
+ *
+ *
+ * @Description : 日志信息记录
+ * ---------------------------------
+ * @Author : deane.administrator
+ * @Date : Create in 2017年12月27日下午4:01:10
+ * 
+ * Copyright (C)2013-2017 小树盛凯科技 All rights reserved.
+ */
+public interface LogService {
+
+	public void save(int userId, String methodName, String params, int costTime, Date requestTime);
+	
+}
