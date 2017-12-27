293604c410e81fef00e2a21a5c0480bc9ecb3627
deane163 <deane163@126.com>
2017-12-27 16:21:33 +0800

    【功能】添加备注信息

--- springboot-apiframework/src/main/java/com/xiaoshu/annotation/Log.java
+++ springboot-apiframework/src/main/java/com/xiaoshu/annotation/Log.java
@@ -8,6 +8,35 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 
+ * code is far away from bug with the animal protecting
+ * ┏┓　　　┏┓
+ * ┏┛┻━━━┛┻┓
+ * ┃　　　　　　　┃
+ * ┃　　　━　　　┃
+ * ┃　┳┛　┗┳　┃
+ * ┃　　　　　　　┃
+ * ┃　　　┻　　　┃
+ * ┃　　　　　　　┃
+ * ┗━┓　　　┏━┛
+ * 　　┃　　　┃神兽保佑
+ * 　　┃　　　┃代码无BUG！
+ * 　　┃　　　┗━━━┓
+ * 　　┃　　　　　　　┣┓
+ * 　　┃　　　　　　　┏┛
+ * 　　┗┓┓┏━┳┓┏┛
+ * 　　　┃┫┫　┃┫┫
+ * 　　　┗┻┛　┗┻┛
+ *
+ *
+ * @Description : 
+ * ---------------------------------
+ * @Author : deane.administrator
+ * @Date : Create in 2017年12月27日下午4:20:48
+ * 
+ * Copyright (C)2013-2017 小树盛凯科技 All rights reserved.
+ */
 @Documented
 @Inherited
 @Target({METHOD})
--- springboot-apiframework/src/main/java/com/xiaoshu/annotation/LogType.java
+++ springboot-apiframework/src/main/java/com/xiaoshu/annotation/LogType.java
@@ -1,5 +1,34 @@
 package com.xiaoshu.annotation;
 
+/**
+ * 
+ * code is far away from bug with the animal protecting
+ * ┏┓　　　┏┓
+ * ┏┛┻━━━┛┻┓
+ * ┃　　　　　　　┃
+ * ┃　　　━　　　┃
+ * ┃　┳┛　┗┳　┃
+ * ┃　　　　　　　┃
+ * ┃　　　┻　　　┃
+ * ┃　　　　　　　┃
+ * ┗━┓　　　┏━┛
+ * 　　┃　　　┃神兽保佑
+ * 　　┃　　　┃代码无BUG！
+ * 　　┃　　　┗━━━┓
+ * 　　┃　　　　　　　┣┓
+ * 　　┃　　　　　　　┏┛
+ * 　　┗┓┓┏━┳┓┏┛
+ * 　　　┃┫┫　┃┫┫
+ * 　　　┗┻┛　┗┻┛
+ *
+ *
+ * @Description : 日志类型
+ * ---------------------------------
+ * @Author : deane.administrator
+ * @Date : Create in 2017年12月27日下午4:20:53
+ * 
+ * Copyright (C)2013-2017 小树盛凯科技 All rights reserved.
+ */
 public enum LogType {
 
 	QUERY,
