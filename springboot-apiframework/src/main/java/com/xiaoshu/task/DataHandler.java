13241436bbdccc913b98f85c3e2f68cb0fe76760
deane163 <deane163@126.com>
2018-01-20 10:27:20 +0800

    【功能】异步处理数据信息

--- springboot-apiframework/src/main/java/com/xiaoshu/task/DataHandler.java
+++ springboot-apiframework/src/main/java/com/xiaoshu/task/DataHandler.java
@@ -2,6 +2,35 @@ package com.xiaoshu.task;
 
 import java.util.List;
 
+/**
+ * 
+ * code is far away from bug with the animal protecting
+ * ┏┓　　　┏┓
+ * ┏┛┻━━━┛┻┓
+ * ┃　　　　　　　┃
+ * ┃　　　━　　　┃
+ * ┃　┳┛　┗┳　┃
+ * ┃　　　　　　　┃
+ * ┃　　　┻　　　┃
+ * ┃　　　　　　　┃
+ * ┗━┓　　　┏━┛
+ * 　　┃　　　┃神兽保佑
+ * 　　┃　　　┃代码无BUG！
+ * 　　┃　　　┗━━━┓
+ * 　　┃　　　　　　　┣┓
+ * 　　┃　　　　　　　┏┛
+ * 　　┗┓┓┏━┳┓┏┛
+ * 　　　┃┫┫　┃┫┫
+ * 　　　┗┻┛　┗┻┛
+ *
+ *
+ * @Description : 数据处理接口，进行数据异步处理的操作。
+ * ---------------------------------
+ * @Author : deane.administrator
+ * @Date : Create in 2018年1月20日上午10:26:20
+ * 
+ * Copyright (C)2013-2018 小树盛凯科技 All rights reserved.
+ */
 public interface DataHandler <T>{
 
 	public void handleData(List<T> T);
