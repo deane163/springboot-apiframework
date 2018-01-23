ade69760ae90d11e89c3576f96fadc8088a79684
deane163 <deane163@126.com>
2018-01-23 09:26:45 +0800

    【功能】添加日志信息

--- springboot-apiframework/src/main/java/com/xiaoshu/config/LoggerConfig.java
+++ springboot-apiframework/src/main/java/com/xiaoshu/config/LoggerConfig.java
@@ -3,6 +3,8 @@ package com.xiaoshu.config;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
@@ -41,6 +43,8 @@ import com.xiaoshu.task.impl.LogTask;
 @Configuration
 @ConditionalOnProperty(prefix ="xiaoshu.log",name="type",havingValue ="async",matchIfMissing=false)
 public class LoggerConfig {
+	
+	private Logger logger = LoggerFactory.getLogger(LoggerConfig.class);
 
 	@Autowired
 	private LogTask logTask;
@@ -48,15 +52,16 @@ public class LoggerConfig {
 	// 启动异步的日志处理操作
 	@PostConstruct
 	public void startHandlerLog(){
+		logger.info("启动异步日志处理，进行日志到数据库的操作。");
 		Thread dataThread = new Thread(logTask);
 		// 作为守护进程进行启动
-		dataThread.setDaemon(true);
+		dataThread.setDaemon(true); // daemon, service jvm, user thread leave >>> daemon leave >>> jvm leave
 		dataThread.start();
 		
 	}
 	
 	@PreDestroy
 	public void destoryHandlerLog(){
-		
+		logger.info("destory the Handler of log records ");
 	}
 }
